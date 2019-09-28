64f9872d15c5e17191e32e281dcf0c4011ba7145
denis281542 <deniskuimov@mail.ru>
2019-09-28 11:36:31 +0500

    Add user name in midel user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def user_name
+    return self.email.split('@')[0].capitalize
+  end
+
 end
