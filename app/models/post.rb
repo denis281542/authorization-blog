06b015b39d4fe7827a0cef54c57561ead833a0e7
denis281542 <deniskuimov@mail.ru>
2019-09-29 15:30:28 +0500

    Error veriable def...

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def self.search_by(@search_term)
+    where("LOWER(name) LIKE :search_term", search_term: "%#{search_term.downcase}%")
+  end
+
 end
